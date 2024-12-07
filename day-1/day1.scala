6a110a252b8246b9d4db2f9c5d416ceb06e2068b
volutus <trevcat3@hotmail.com>
2024-12-06 22:34:25 -0500

    Cleanup / consolidation

--- day-1/day1.scala
+++ day-1/day1.scala
@@ -6,54 +6,45 @@ object day1
 {
     def main(args: Array[String]) = 
     {   
+        val (leftList, rightList) = readFile()
+        var sum = 0
+        var similarity = 0
+        for ( (left, right) <- (leftList zip rightList))
+        {
+            val diff = Math.abs(left - right)
+            sum += diff
+            
+            val freq = rightList.count(_ == left)
+            similarity += (freq * left)
+        }
+        println("Part 1: " + sum)
+        println("Part 2: " + similarity)
+    }
+
+    def readFile() : (List[Int], List[Int]) = 
+    {
         val source = scala.io.Source.fromFile("day-1/input.txt")
+        val leftBuffer = new mutable.ListBuffer[Int]
+        val rightBuffer = new mutable.ListBuffer[Int]
         try 
         {
             // Part 1: Difference score
-            var left = new mutable.ListBuffer[Int]
-            var right = new mutable.ListBuffer[Int]
             val lineIterator = source.getLines()
             for (line <- lineIterator)
             {
                 val parts = line.split(Delimiter)
                 
                 val leftValue = parts(0)
-                left.addOne(leftValue.toInt)
+                leftBuffer.addOne(leftValue.toInt)
                 
                 val rightValue = parts(1)
-                right.addOne(rightValue.toInt)
+                rightBuffer.addOne(rightValue.toInt)
             }
-            left = left.sorted
-            right = right.sorted
-
-            var i = 0
-            var sum = 0
-            val length = Math.min(left.length, right.length)
-            while (i < length)
-            {
-                val leftValue = left(i)
-                val rightValue = right(i)
-
-                val diff = Math.abs(leftValue - rightValue)
-                sum += diff
-                i += 1
-            }
-
-            println("Part 1: " + sum)
-
-            // Part 2: Similarity score
-            i = 0 
-            var similarity = 0
-            while (i < length)
-            {
-                val value = left(i)
-                val freq = right.count(_ == value)
-                similarity += (freq * value)
-                i += 1
-            }
-
-            println("Part 2: " + similarity)
         }
         finally source.close()
+
+        val left = leftBuffer.sorted.toList
+        val right = rightBuffer.sorted.toList
+        return (left, right)
     }
 }
